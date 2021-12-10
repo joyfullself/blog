6a66ef8f5662643821b5e3e1532107d164183eba
Shawn <2001401@sit.singaporetech.edu.sg>
2021-12-11 07:25:59 +0800

    Corrected Component Name

--- components/index.js
+++ components/index.js
@@ -1,3 +1,3 @@
 export { default as PostCard } from './PostCard'
 export { default as Categories } from './Categories'
-export { default as PostWidgets } from './PostWidgets'
+export { default as PostWidget } from './PostWidget'
