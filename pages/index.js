526427c61266cef7ec5bd5a81a98c274a693d650
Shawn <2001401@sit.singaporetech.edu.sg>
2021-12-11 01:20:58 +0800

    Test

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+
+const posts = [
+  { title: 'Post1', excerpt: 'Post1 Excerpt'},
+  { title: 'Post2', excerpt: 'Post2 Excerpt'},
+];
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Joyfullself</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post,index) => (
+            <div>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+            <div className="lg:sticky relative top-8">
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
