57a72771f7e9b9bbcd0d9274bbfc11654ca71c06
Shawn <2001401@sit.singaporetech.edu.sg>
2021-12-13 11:48:06 +0800

    Imperium CTA

--- pages/imperium.js
+++ pages/imperium.js
@@ -1,6 +1,8 @@
 // Scroll by a component's ref
 import React, { Component } from "react";
 import { ScrollTo } from "react-scroll-to";
+import ScrollToTop from "react-scroll-to-top";
+// import ScrollingWrapper from '../Components/ScrollingWrapper'
 
 
 
@@ -9,7 +11,7 @@ export default class Imperium extends Component {
     render() {
         return (
             <div className="container mx-auto mt-4 px-8 shadow-lg rounded-lg py-8">
-
+                {/* <ScrollingWrapper> */}
                 <div className="bg-white shadow-lg rounded-lg">
                     <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
                         <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
@@ -30,7 +32,7 @@ export default class Imperium extends Component {
                             {({ scroll }) => (
                                 <a
                                 onClick={() => scroll({x: 20, y: 2000, smooth: true })}
-                                className=" cursor-pointer inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
+                                className="cursor-pointer inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
                                 >
                                 Learn more
                                 </a>
@@ -51,7 +53,7 @@ export default class Imperium extends Component {
                 </div>
 
                 <div className="bg-white shadow-lg rounded-lg py-8 mt-8" >
-                    <div className="grid grid-cols-1 lg:grid-cols-5 gap-6 justify-items-center px-8">        
+                    <div className="grid grid-cols-1 lg:grid-cols-5 gap-6 justify-items-center px-8">
                         <div className="lg:col-span-6 col-span-1 ml-5 mt-4">
                             <h1 className="text-2xl font-semibold text-gray-700">Testimonies</h1>
                         </div>
@@ -70,11 +72,11 @@ export default class Imperium extends Component {
                     </div>
                 </div>
 
-                <div className="bg-white rounded-xl py-8 mt-8" >
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 justify-items-center">                            
+                <div className="bg-white rounded-xl mx-auto mt-8" >
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 justify-items-center">
                         <div className="rounded-lg shadow-lg">
                             <img
-                            className="object-fill rounded-lg mx-auto"
+                            className="object-fill rounded-lg"
                             src="imperium_1.png"
                             height="100%"
                             width="100%"
@@ -82,19 +84,15 @@ export default class Imperium extends Component {
                         </div>
                         <div className="rounded-lg shadow-lg">
                             <img
-                            className="object-fill  rounded-2xl mx-auto"
+                            className="object-fill rounded-lg"
                             src="imperium_2.png"
                             height="100%"
                             width="100%"
                             />
                         </div>
-
-                        <div className="rounded-lg shadow-lg">
-                                Scroll To Top
-                        </div>
-
                     </div>
                 </div>
+                {/* </ScrollingWrapper> */}
             </div>
         )
     }
